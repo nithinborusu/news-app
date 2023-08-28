9aa431b2ba59a100a934c03bc80f3f1bf4719c25
Nithin Borusu <nithinborusu23@gmail.com>
2023-08-29 00:27:51 +0530

    Added express to application

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("mikasa");
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", function (req, res) {
+  res.send("mikasaa!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
